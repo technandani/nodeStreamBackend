e4ccb00221a6af0336f8f0904c14ae44941268c7
Nandani <nks854338@gmail.com>
2024-10-18 15:54:35 +0530

    commited

--- index.js
+++ index.js
@@ -1,31 +1,54 @@
 const express = require('express');
-const cors = require('cors');
+const fs = require('fs');
 const app = express();
+const cors = require('cors');
+const path = require('path');
 const port = 5000;
 
 app.use(cors());
-
-const videoUrls = [
-  "https://res.cloudinary.com/dpmengi5q/video/upload/v1729100776/multer_y08vdz.mp4",
-  "https://res.cloudinary.com/dpmengi5q/video/upload/v1729100777/nodejs_btnaoo.mp4",
-  "https://res.cloudinary.com/dpmengi5q/video/upload/v1729100777/nodestream_zgnubu.mp4"
-];
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Video Streaming Service");
 });
 
-app.get("/videos", (req, res) => {
-  res.json(videoUrls); 
-});
+app.get("/video/:videoName", (req, res) => {
+  const videoPath = path.join(__dirname, 'videos', `${req.params.videoName}.mp4`);
 
-app.get("/video/:videoIndex", (req, res) => {
-  const videoIndex = parseInt(req.params.videoIndex, 10);
-  if (isNaN(videoIndex) || videoIndex < 0 || videoIndex >= videoUrls.length) {
-    return res.status(404).send("Video not found");
-  }
+  fs.stat(videoPath, (err, stat) => {
+    if (err || !stat) {
+      return res.status(404).send("File not found");
+    }
 
-  res.redirect(videoUrls[videoIndex]);
+    const fileSize = stat.size;
+    const range = req.headers.range;
+    if (!range) {
+      const headers = {
+        "Content-Length": fileSize,
+        "Content-Type": "video/mp4",
+      };
+
+      res.writeHead(200, headers);
+      fs.createReadStream(videoPath).pipe(res);
+      return;
+    }
+
+    const chunkSize = 10 ** 6;
+    const start = Number(range.replace(/\D/g, ""));
+    const end = Math.min(start + chunkSize, fileSize - 1);
+
+    const contentLength = end - start + 1;
+    const headers = {
+      "Content-Range": `bytes ${start}-${end}/${fileSize}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": contentLength,
+      "Content-Type": "video/mp4",
+    };
+
+    res.writeHead(206, headers);
+
+    const fileStream = fs.createReadStream(videoPath, { start, end });
+    fileStream.pipe(res);
+  });
 });
 
 app.listen(port, () => {
